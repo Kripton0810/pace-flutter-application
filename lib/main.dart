35f3e8338f399cc7851d284794b6e45045d4ee93
Kripton0810 <subhankar0810@gmail.com>
2022-10-04 17:59:55 +0530

    top box

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xff00A9B7),
+    ),
+  );
   runApp(HomePage());
 }
 
@@ -15,12 +21,52 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter App"),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      theme: ThemeData(
+        primaryColor: Color(0xff00A9B7),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xff00A9B7),
         ),
-        body: SafeArea(child: Text("Hello world")),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              child: Container(
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text("Hello world"),
+                        Text("Fuck you"),
+                      ],
+                    )
+                  ],
+                ),
+                decoration: const BoxDecoration(
+                  color: Color(0xff00A9B7),
+                  image: DecorationImage(
+                    image: AssetImage('assets/header_waves_png.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(28),
+                    bottomRight: Radius.circular(28),
+                  ),
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text("data"),
+                ],
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
