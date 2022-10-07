5c4ed6e27a86eee82d3514f2c696f1d2bb4cce3e
Kripton0810 <subhankar0810@gmail.com>
2022-10-08 01:58:06 +0530

    widget added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pace_classes_mobile_application/components/course_home_page-display.dart';
 import 'package:pace_classes_mobile_application/components/header.dart';
 
 void main() {
@@ -74,127 +75,12 @@ class _HomePageState extends State<HomePage> {
                       scrollDirection: Axis.horizontal,
                       itemCount: 3,
                       itemBuilder: (context, item) {
-                        return UnconstrainedBox(
-                          child: Container(
-                            margin: EdgeInsets.symmetric(horizontal: 20),
-                            width: 220,
-                            child: Column(
-                              children: [
-                                //stack is for love widget stars and image
-                                Stack(
-                                  children: [
-                                    Image(
-                                      image: AssetImage(
-                                        'assets/pic_for_background.png',
-                                      ),
-                                      width: 220,
-                                      height: 140,
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 16),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Container(
-                                            margin: EdgeInsets.only(left: 16),
-                                            width: 49,
-                                            height: 28,
-                                            decoration: BoxDecoration(
-                                              color: Colors.white,
-                                              borderRadius:
-                                                  BorderRadius.circular(8),
-                                            ),
-                                            child: Center(
-                                              child: Row(
-                                                children: [
-                                                  Image.asset(
-                                                      "assets/star_image.png"),
-                                                  SizedBox(
-                                                    width: 4,
-                                                  ),
-                                                  Text(
-                                                    "4.8",
-                                                    style: TextStyle(
-                                                        fontFamily: "DMSans"),
-                                                  )
-                                                ],
-                                              ),
-                                            ),
-                                          ),
-                                          Container(
-                                            margin: EdgeInsets.only(right: 16),
-                                            width: 28,
-                                            height: 28,
-                                            decoration: BoxDecoration(
-                                              color: Colors.white,
-                                              borderRadius:
-                                                  BorderRadius.circular(8),
-                                            ),
-                                            child: Image.asset(
-                                              "assets/heart.png",
-                                              color: Color.fromARGB(
-                                                  255, 163, 153, 153),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                Container(
-                                  width: 240,
-                                  margin: EdgeInsets.only(top: 16),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    // ignore: prefer_const_literals_to_create_immutables
-                                    children: [
-                                      Text(
-                                        "Design Thinking Fundamental",
-                                        style: TextStyle(
-                                          fontFamily: "DMSans",
-                                          fontSize: 14,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 8,
-                                      ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        children: [
-                                          Image.asset(
-                                            "assets/instructor.png",
-                                          ),
-                                          SizedBox(
-                                            width: 4,
-                                          ),
-                                          Text(
-                                            "Author name",
-                                            style: TextStyle(
-                                              fontFamily: "DMSans",
-                                              fontSize: 12,
-                                              color: Color(0xffA9AEB2),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      Text(
-                                        "₹105",
-                                        style: TextStyle(
-                                          fontFamily: "DMSans",
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w700,
-                                          color: Color(0xff00A9B7),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                        return CourseHomePageDisplay(
+                          courseAuthor: "Subhankar Dutta",
+                          courseId: item.toString(),
+                          courseName: "Data Science",
+                          coursePrice: 500,
+                          rating: 3.4,
                         );
                       },
                     ),
